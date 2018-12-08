39f2de02b8c0b183c395d47db012bbf2ecf1d150
jrushkoff <45719211+jrushkoff@users.noreply.github.com>
2018-12-08 13:34:22 -0500

    Add files via upload

---
+++ ThermoProject.py
@@ -0,0 +1,12 @@
+##EDIT ME
+const NUMBER_OF_TANKS = 3 #Change 3 to the number of tanks you have
+const VOLUME_OF_TANKS = 18000 #Change 18000 to the size of tanks you have in gallons
+
+
+##DO NOT TOUCH!! IF YOU DO, YOU WILL HAVE TO REDOWNLOAD THE PROGRAM UNLESS YOU HAPPEN TO KNOW HOW TO CODE.
+const ACTIVITY_COEFFICIENT = .5
+const SATURATION_PRESSURE_OF_GASOLINE = 100
+
+def psiToPa(pressure):
+    return pressure
+
